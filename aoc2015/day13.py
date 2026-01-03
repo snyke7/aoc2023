5a4b48a0cdaf4aac0474928bb737de7d26490de1
Ike Mulder <ikemulder@hotmail.com>
2026-01-03 14:26:46 +0000

    AoC 2015 day 13
    Signed-off-by: Ike Mulder <ikemulder@hotmail.com>

--- aoc2015/day13.py
+++ aoc2015/day13.py
@@ -1,3 +1,7 @@
+from collections import defaultdict
+from itertools import permutations
+
+
 TEST_INPUT = '''Alice would gain 54 happiness units by sitting next to Bob.
 Alice would lose 79 happiness units by sitting next to Carol.
 Alice would lose 2 happiness units by sitting next to David.
@@ -15,12 +19,54 @@ David would gain 41 happiness units by sitting next to Carol.
 
 def parse_line(the_line):
     name, _, tail = the_line.partition(' would ')
-    diff, _, the_name = tail.partition(' happiness units by sitting next to ')
-    
+    diff_str, _, the_name = tail.strip()[:-1].partition(' happiness units by sitting next to ')
+    diff_sign, _, diff_amt_str = diff_str.partition(' ')
+    diff_amt = int(diff_amt_str) * (1 if diff_sign == 'gain' else -1)
+    return name, the_name, diff_amt
+
+
+def get_happiness_dict(test_input):
+    result_dict = defaultdict(lambda: 0)
+    names = set()
+    for left, right, diff in (parse_line(line) for line in test_input.splitlines()):
+        names.add(left)
+        names.add(right)
+        result_dict[frozenset((left, right))] += diff
+    return list(sorted(names)), dict(result_dict)
+
+
+def get_happiness(comb, neighbor_score):
+    result = 0
+    for left, right in zip(comb, comb[1:] + [comb[0]]):
+        result += neighbor_score[frozenset((left, right))]
+    return result
+
+
+def get_best_seating(names, neighbor_score):
+    best_happiness = -1
+    best_comb = None
+    for comb_tail in permutations(names[1:], len(names) - 1):
+        comb = [names[0]] + list(comb_tail)
+        this_happiness = get_happiness(comb, neighbor_score)
+        if this_happiness > best_happiness:
+            best_happiness = this_happiness
+            best_comb = comb
+    return best_happiness
+
+
 
 
 def main():
-    test_input = TEST_INPUT.splitlines()
+    test_input = TEST_INPUT
+    with open('input/day13.txt') as f:
+        test_input = f.read()
+    names, neighbor_score = get_happiness_dict(test_input)
+    print(get_best_seating(names, neighbor_score))
+    # part 2
+    for name in names:
+        neighbor_score[frozenset((name, 'Ike'))] = 0
+    names.append('Ike')
+    print(get_best_seating(names, neighbor_score))
 
 
 
